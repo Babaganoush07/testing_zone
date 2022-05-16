38d52f9026934f53782eb8b0b4618116c485d5c2
Babaganoush07 <94538153+Babaganoush07@users.noreply.github.com>
2022-05-16 16:10:58 -0700

    Update TTGOCalculator.ino

--- TTGOCalculator.ino
+++ TTGOCalculator.ino
@@ -1,3 +1,8 @@
+/*
+Using a TTTGO T-Display
+Followed this YouTube video
+https://youtu.be/spkWTnRe6KU
+*/
 #include <TFT_eSPI.h>
 TFT_eSPI tft = TFT_eSPI();
 
@@ -15,9 +20,8 @@ int debounce = 0;
 int debounce2 = 0;
 
 void setup() {
-  
-  pinMode(0, INPUT_PULLUP);
-  pinMode(35, INPUT_PULLUP);
+  pinMode(0, INPUT_PULLUP); // LEFT BUTTON
+  pinMode(35, INPUT_PULLUP); // RIGHT BUTTON
   
   tft.init();
   tft.fillScreen(TFT_BLACK);
@@ -48,11 +52,11 @@ void loop() {
       chosen += 1;
       if(chosen>11){
         chosen = 0;
-      }
+      } // END chosen>11 IF
       tft.drawRect(startX-4, startY-1+(19*chosen),20,18, TFT_BLUE);
       delay(400);
-    }
-  }else debounce = 0;
+    } // END DEBOUNCE IF
+  }else debounce = 0; // END digitalRead(35) IF
 
   if(digitalRead(0)==0){
     if(debounce2==0){
@@ -64,24 +68,24 @@ void loop() {
       if(chosen == 10){
         numbers[fase]="";
         tft.drawString("      ", 22, 50+(46*fase), 4);
-      }
+      } // END CHOSEN IF
 
       if(chosen == 11){
         fase+=1;
         tft.drawLine(4, 130, 100, 130, TFT_BLUE);
         tft.drawString("+", 0, 98, 6);
-      }
+      } // END CHOSEN IF
       
       if(fase == 2){
         int result = numbers[0].toInt() + numbers[1].toInt();
         numbers[fase] = String(result);
         //tft.drawLine(4, 138, 100, 138, TFT_WHITE);
         //tft.drawString("+", 0, 98, 4);
-      }
+      } // END FASE IF
 
       tft.drawString(numbers[fase], 22, 50+(46*fase), 4);
       delay(400);
-    }
-  }else debounce2 = 0;
+    } // END DEBOUNCE2
+  }else debounce2 = 0; // END digitalRead(35) IF
 
-}
+} // END LOOP()
