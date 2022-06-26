ea7404b94e7c2f93d9203642f8151927a52624a5
Babaganoush07 <94538153+Babaganoush07@users.noreply.github.com>
2022-06-26 09:35:54 -0700

    Update dimableLED.ino

--- esp32Tutorials/dimableLED.ino
+++ esp32Tutorials/dimableLED.ino
@@ -1,4 +1,5 @@
 #define potPIN 15
+
 #define LED_PIN 23
 const int channel = 0;
 const int frequency = 1000;
@@ -10,11 +11,10 @@ int brightness;
 void setup() {
   ledcSetup(channel,frequency,resolution);
   ledcAttachPin(LED_PIN,channel);
-}
+} // END setup()
 
 void loop() {
   potValue = analogRead(potPIN);
   brightness = (255. / 4095.) * potValue;
-
   ledcWrite(channel, brightness);
-}
+} // END loop()
